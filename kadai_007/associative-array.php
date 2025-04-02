5e4b10f5cad1fa1eb1be882ece4f1850e3fe0d27
Kimi <kimiko.takanabe@gmail.com>
2025-04-02 13:01:46 +0200

    kadai_007フォルダをアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>kadai_007</title>
+</head>
+<body>
+  <p>
+    <?php 
+     // 連想配列に値を代入する
+    $product = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    // 連想配列の値を出力する
+    print_r($product);
+    ?>
+  </p>
+</body>
+</html>
